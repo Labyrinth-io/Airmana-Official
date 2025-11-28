d1ad2275bc1364c30dc4c1a981d00a8a7d511af1
Labyrinth-io <mmaxryann4@gmail.com>
2025-11-28 14:27:25 +1000

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 import { Menu, X } from 'lucide-react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import airmanaLogo from './images/Airmanalogotransparent.webp';
+import airmanaLogotransparent from './images/Airmanalogotransparent.webp';
 
 export default function Hero() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
