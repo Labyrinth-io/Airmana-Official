c97ce1d2f2910c42f3ea5d5dd2710a8892706507
Labyrinth-io <mmaxryann4@gmail.com>
2025-11-28 14:43:52 +1000

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -54,7 +54,7 @@ export default function Hero() {
           </div>
         )}
       </nav>
- 
+
       <div className="relative h-screen flex items-center justify-center bg-[#0B1120]">
         <div className="relative z-20 flex flex-col items-center justify-center w-full px-6 pt-32 md:pt-24 pb-16 md:pb-24">
           <div className="w-full max-w-md mx-auto mb-4 md:mb-0">
